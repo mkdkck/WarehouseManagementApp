39e41e48cf777fc62e31c62ca26431347760b9fc
mkdkck <colin173@gmail.com>
2024-02-18 22:58:22 +1030

    added global context codes, debuged from server to client, now the server can run to the home page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Home from './pages/Home';
 import Error from './pages/Error';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    error: <Error />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Home />
-      }, {
-        path: '/login',
-        element: <Login />
-      }, {
-        path: '/signup',
-        element: <Signup />
       },
+      //  {
+      //   path: '/login',
+      //   element: <Login />
+      // }, {
+      //   path: '/signup',
+      //   element: <Signup />
+      // },
     ]
   }
 ]);
---
+++ server/models/index.js
@@ -0,0 +1,10 @@
+const User = require('./User');
+// const Category = require('./Category');
+// const Product = require('./Product');
+// const Warehouse = require('./Warehouse');
+
+
+module.exports = {
+    User,
+    //  Product, Category, Warehouse
+};
